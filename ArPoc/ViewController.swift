ad48f66bad9ef464c1d41abdc15fec9addc8e35a
collinhemeltjen <collinhemeltjen@gmail.com>
2019-11-27 09:38:42 +0100

    initial commit

---
+++ ArPoc/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  ArPoc
+//
+//  Created by Collin Hemeltjen on 19/11/2019.
+//  Copyright © 2019 Collin Hemeltjen. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+import ARKit
+import Combine
+
+class ViewController: UIViewController {
+
+	@IBOutlet var arView: ARView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		addTapGestureToSceneView()
+	}
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		enablePeopleOcclusion()
+	}
+
+	func addVase(on anchor: float4x4) {
+		var cancellable: AnyCancellable? = nil
+		cancellable = ModelEntity.loadModelAsync(named: "vase")
+			.sink(receiveCompletion: { error in
+				print("Unexpected error: \(error)")
+				cancellable?.cancel()
+			}, receiveValue: { entity in
+				let anchorEntity = AnchorEntity(world: anchor)
+				self.arView.scene.anchors.append(anchorEntity)
+
+				entity.scale = [1, 1, 1] * 0.006
+				entity.generateCollisionShapes(recursive: true)
+				anchorEntity.children.append(entity)
+			})
+	}
+
+	func addTapGestureToSceneView() {
+		let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+		arView.addGestureRecognizer(tapGestureRecognizer)
+	}
+
+	@objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+		let tapLocation = recognizer.location(in: arView)
+
+		guard let entity = arView.entity(at: tapLocation) else {
+			let hitTestResultsWithFeaturePoints = arView.hitTest(tapLocation, types: .estimatedHorizontalPlane)
+			if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+				addVase(on: hitTestResultWithFeaturePoints.worldTransform)
+			}
+			return
+		}
+		entity.removeFromParent()
+	}
+
+	func enablePeopleOcclusion(){
+		guard ARWorldTrackingConfiguration.supportsFrameSemantics(.personSegmentationWithDepth) else {
+			print("People occlusion is not supported on this device.")
+			return
+		}
+		guard let config = arView.session.configuration else {
+			fatalError("Unexpectedly failed to get the configuration.")
+		}
+		config.frameSemantics.insert(.personSegmentationWithDepth)
+		arView.session.run(config)
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+![Liveview demo](image.png)
+
 # TulipPlacer
 An ArKit/RealityKit app to place Tulips in the real world. AR PoC
+
+![Liveview demo](preview.gif)
